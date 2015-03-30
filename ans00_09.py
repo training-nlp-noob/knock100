40b6d1c1c58336bfd36cea19eacf3a712919c1d9
cipron81 <cipron81@users.noreply.github.com>
2015-03-30 16:28:52 +0900

    Q4

--- ans00_09.py
+++ ans00_09.py
@@ -69,6 +69,23 @@ for i in  range(len(words)) :
      print(i,words[i], len(words[i]))
 
 
+#4 
+print("Q4")
+
+hoge = "Hi He Lied Because Boron Could Not Oxidize Fluorine. New Nations Might Also Sign Peace Security Clause. Arthur King Can."
+words= hoge.split( " ")
+
+dic = {}
+for i in range(len(words)):
+    if i in (0, 4, 5, 6, 7, 8, 14, 15, 18):
+        print(words[i],words[i][0:1])
+        dic[words[i][0:1]] = i+1
+    else:
+        print (words[i], words[i][0:2])
+        dic[words[i][0:2]] = i+1
+print(dic)
+
+
 #05. n-gram
 
 print("第5問")
