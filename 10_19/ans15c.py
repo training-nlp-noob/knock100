48e9962757dfd92b3b66f7eab606af22d46c3ca1
cipron81 <cipron81@users.noreply.github.com>
2015-04-09 23:56:36 +0900

    Q15start

--- 10_19/ans15c.py
+++ 10_19/ans15c.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python
 # coding=utf-8
 
+
 #http://www.yukun.info/blog/2008/07/python-command-line-arguments.html
+
+
+import sys # モジュール属性 argv を取得するため
+
+argvs = sys.argv  # コマンドライン引数を格納したリストの取得
+argc = len(argvs) # 引数の個数
+# デバッグプリント
+print argvs
+print argc
