e4bd872a80169a646a940b4b57104f61aa8f4787
kunisawa-fc <kunisawa-fc@users.noreply.github.com>
2015-04-11 08:33:35 +0900

    Update ans15c.py

--- 10_19/ans15c.py
+++ 10_19/ans15c.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 # coding=utf-8
 
+#15. 末尾のN行を出力
+#自然数Nをコマンドライン引数などの手段で受け取り，入力のうち末尾のN行だけを表示せよ．確認にはtailコマンドを用いよ．
+
 
 #http://www.yukun.info/blog/2008/07/python-command-line-arguments.html
 
