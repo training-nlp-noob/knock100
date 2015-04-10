7ad0bd1db8072f0259461a3a27610367386875b2
kunisawa-fc <kunisawa-fc@users.noreply.github.com>
2015-04-11 08:15:59 +0900

    Update ans15c.py
    すんません、１４が間違って１５してたので、書かせてもらいます。

--- 10_19/ans15c.py
+++ 10_19/ans15c.py
@@ -8,7 +8,37 @@
 import sys # モジュール属性 argv を取得するため
 
 argvs = sys.argv  # コマンドライン引数を格納したリストの取得
-argc = len(argvs) # 引数の個数
+#「sys.argv」に起動時のパラメータが格納されます。
+#リストで値が返されますが、最初の値には実行ファイル名が入りますので注意しましょう。
+#http://www.python-izm.com/contents/basis/command_line_arguments.shtml
+
 # デバッグプリント
-print argvs
-print argc
+print("渡されたパラメーターを一応確認表示です")
+print (argvs)
+print("引数の個数")
+argc = len(argvs) # 引数の個数
+print (argc)
+
+#エラー処理を入れておきます
+#http://symfoware.blog68.fc2.com/blog-entry-873.html
+
+#なんと、先日考察した、readlinesがNULLになるのがここで生きます
+#行数をとるのと読むのと、二つを回します
+try:
+    x = int(argvs[1])
+    f1 = open ("./data/hightemp.txt","r",encoding="utf-8")
+    f2 = open ("./data/hightemp.txt","r",encoding="utf-8")
+    with f1, f2:
+        linen = len(f1.readlines())
+        print("全部で" + str(linen) + "行。" + str(argvs[1]) + "行前からから末まで表示")
+        lines = f2.readlines()
+        for y in range(0,x):
+            print(lines[linen-x+y])
+except ValueError:
+    print ("整数を入力してください")
+
+
+#Windows ではコマンドラインで　python ans15c.py 3 で実行します
+
+#　UNIXでは、　tail -n N hightemp.txt で確認できます。はい。
+
