6df4d83bc0cb6c6ddb6d1e8d75c95fdf9fdd6eb8
kunisawa-fc <kunisawa-fc@users.noreply.github.com>
2015-04-11 08:28:44 +0900

    Update ans14k.py
    初めから数行に改訂

--- 10_19/ans14k.py
+++ 10_19/ans14k.py
@@ -1,4 +1,8 @@
 # -*- coding: utf-8 -*-
+
+#14. 先頭からN行を出力
+#自然数Nをコマンドライン引数などの手段で受け取り，入力のうち先頭のN行だけを表示せよ．確認にはheadコマンドを用いよ．
+
 import sys
 
 parm = sys.argv
@@ -19,14 +23,18 @@ try:
     f2 = open ("./data/hightemp.txt","r",encoding="utf-8")
     with f1, f2:
         linen = len(f1.readlines())
-        print("全部で" + str(linen) + "行。" + str(parm[1]) + "行前からから末まで表示")
-        lines = f2.readlines()
-        for y in range(0,x):
-            print(lines[linen-x+y])
+        print("全部で" + str(linen) + "行。" + str(parm[1]) + "行まで表示")
+        if x <= linen:
+            lines = f2.readlines()
+            for y in range(0,x): #最初は含む 最後は含まない・・・わかりにくい
+                #http://atkonn.blogspot.jp/2008/02/python-python21-range.html
+                print(lines[y])
+        else:
+            print(str(linen) + "までの自然数を入れてください")
 except ValueError:
-    print ("整数を入力してください")
+    print ("自然数を入力してください")
 
 
 #Windows ではコマンドラインで　python ans14k.py 3 で実行します
 
-#　UNIXでは、　tail -n N hightemp.txt で確認できます。はい。
+#　UNIXでは、　head -n N hightemp.txt で確認できます。はい。
