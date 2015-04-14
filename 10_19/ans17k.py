951bbb6e99a307e68a68522baa43724f34d5ef60
kunisawa-fc <kunisawa-fc@users.noreply.github.com>
2015-04-15 00:10:58 +0900

    Update ans17k.py
    うぉ、0時少し過ぎたけど許して

--- 10_19/ans17k.py
+++ 10_19/ans17k.py
@@ -12,8 +12,17 @@ ls = []
 for i in range(len(ff)):
     col1 = ff[i].split("\t")[0]
     ls.append(col1)
+print(set(ls))
+
+#ついでに、違う並びかたも
 lsu = sorted(set(ls), key=ls.index)
 #重複削除 set　に、出現順を追加 sorted
 #http://d.hatena.ne.jp/t-fridge/20080323/1206217930
 print(lsu)
 print('1列目、一意の一覧（出現順）・個数；'+str(len(lsu))+'個')
+
+
+#UNIXだと
+# cut -d $'\t' -f 1 hightemp.txt | sort | uniq
+
+#以上（笑）　uniq　が sort　と一緒でなくては使えない
