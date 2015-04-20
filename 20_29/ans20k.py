825ffbae531fd769966697f14cc3e8f45f4fcc5b
cipron81 <cipron81@users.noreply.github.com>
2015-04-20 10:23:10 +0900

    Revert "Q20modified" revert training This reverts commit b2996e8878016690c567f43f68a9f72a32df301c.

--- 20_29/ans20k.py
+++ 20_29/ans20k.py
@@ -7,27 +7,8 @@
 import gzip
 import json
 
-#with gzip.open("./data/jawiki-country.json.gz", 'r') as wiki:
-#    for line in wiki :
-#        print(wiki.readlines())
-### json.dumps
-## json.loads とか使うんだろうけど
-
 with gzip.open("./data/jawiki-country.json.gz", 'rt',encoding="utf-8") as wiki:
-    for line in wiki:
-        json_data=json.loads(line)
-        print(json.dumps(json_data,sort_keys = True, indent = 4, ensure_ascii=False))
+  print('ここからわからん・・')
 
-
-# http://peaceandhilightandpython.hatenablog.com/entry/2013/12/06/082106
-# json.dump すると文字化けする
-# http://d.hatena.ne.jp/tatz_tsuchiya/20120227/1330325015 既知の問題
-# 読み込むところまでは出来た
-
-with gzip.open("./data/jawiki-country.json.gz", 'rt',encoding="utf-8") as wiki:
-    for line in wiki:
-        json_data=json.loads(line)
-        if json_data["title"] == "イギリス":
-            print(json.dumps(json_data,sort_keys = True, indent = 4, ensure_ascii=False))
-        else:
-            print("aaa")
+# json.dumps
+# json.loads とか使うんだろうけど
