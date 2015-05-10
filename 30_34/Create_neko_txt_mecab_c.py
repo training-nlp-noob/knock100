71a66f163cfbda78e3f62a3af79c0af23970f936
kunisawa-fc <kunisawa-fc@users.noreply.github.com>
2015-05-10 11:36:15 +0900

    Update Create_neko_txt_mecab_c.py

--- 30_34/Create_neko_txt_mecab_c.py
+++ 30_34/Create_neko_txt_mecab_c.py
@@ -2,13 +2,18 @@
 # -*- coding: utf-8 -*-
 # create neko.txt.mecab
 
+#問題文では、どうもデフォルトのmecabrcで読み込む必要がありそうよ。品詞細分類1とかがいるから。
 
-import sys,MeCab
 
-with open ("./data/neko.txt","r",encoding="utf-8") as neko:
-    with open ("./data/neko.txt.c.mecab","a") as out:
+import MeCab #,sys
+
+with open ("../data/neko.txt","r",encoding="utf-8") as neko, \
+     open ("../data/neko.txt.c.mecab","w",encoding="utf-8") as out: #モードをaからwに変更
         f = neko.read()
-        m = MeCab.Tagger ("-Ochasen")
+        m = MeCab.Tagger() #デフォルト("mecabrc")、茶筅は("-Ochasen")
+        out.write(m.parse(f))
+        '''
+        要素分けは次の問題
         node = m.parseToNode(f)
         while node:
             out.write(node.surface)
@@ -16,6 +21,7 @@ with open ("./data/neko.txt","r",encoding="utf-8") as neko:
             out.write(node.feature)
             out.write("\n")
             node = node.next
+        '''
 
 
 # parse ではうまくいったけど、parseToNodeではうまくいかないな
