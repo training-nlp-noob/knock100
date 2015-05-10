7a0252188420ded449c5b334f53897393666a61b
kunisawa-fc <kunisawa-fc@users.noreply.github.com>
2015-05-10 13:20:23 +0900

    Update and rename 31k_feturing_y.py to ans31k_feturing_y.py
    少しいらん処置をしてみた。
    抽出するのに、printするといくらたっても終わらん。windowsのpythonは、どうもprintが苦手っぽい・・・

---
+++ 30_34/ans31k_feturing_y.py
@@ -0,0 +1,23 @@
+#coding:utf-8
+
+import module30y_k
+print("----------")
+verb = []
+# 品詞が動詞なら、表層形を表示
+for item in module30y_k.listx:
+    if item["pos"]=="動詞":
+        verb.append(item["surface"])
+
+print("サンプルで、100個ほど表示")
+print(verb[:99])
+
+#一応確認用に表層形と原型を格納してみる
+verb2 = []
+for item in module30y_k.listx:
+    if item["pos"]=="動詞":
+        verb2.append([item["surface"],item["base"]] )
+print("----------")
+print("サンプルで、原型とセットで50個ほど表示")
+for i in verb2[:49]:
+    print(i)
+
