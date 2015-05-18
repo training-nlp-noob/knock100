125d38babd4502820fc37c6baf465b36324e9a72
cipron81 <cipron81@users.noreply.github.com>
2015-05-18 13:01:01 +0900

    dir_name_change

---
+++ 30_39/module30y_k.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+import MeCab,re
+
+# neko.txt.mecabの読み込み
+with open("../data/neko.txt.mecab", encoding="utf8") as f:
+    text=f.readlines() # 表層形\t品詞,品詞細分類1,品詞細分類2,品詞細分類3,活用形,活用型,原形,読み,発音（読み、発音はないものも存在）
+
+
+# 初期化
+pattern=re.compile(r"(?P<surface>.+)\t(?P<pos>[^,]+),(?P<pos1>[^,]+),(?P<pos2>[^,]+),(?P<pos3>[^,]+),(?P<conjForm>[^,]+),(?P<conjType>[^,]+),(?P<base>[^,]+),?(.*)")
+listx=[] #listが予約語っぽいので、名前を変えました
+dic={}
+
+# list[dic[0],dic[1],dic[2],...]形式で形態素を収納
+for i,line in enumerate(text):
+    if line=="EOS\n":
+        break
+    dic[i]={}
+    surface,pos,pos1,base=pattern.match(line).group("surface","pos","pos1","base")
+    dic[i]["surface"]=surface
+    dic[i]["pos"]=pos
+    dic[i]["pos1"]=pos1
+    dic[i]["base"]=base
+    listx.append(dic[i])
+
+print("隊長！　listx に格納しました！")
